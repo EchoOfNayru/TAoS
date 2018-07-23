b3ef018ffd8703ca8ffdb67befd30d844a77d27c
Jacob M Miller <s179063@students.aie.edu.au>
2018-07-23 15:12:09 -0700

    Breakable Hammer, and pedistal created

---
+++ Assets/Jacob/Scripts/Breakable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour {
+
+    public GameObject remains;
+    Vector3 currentSpeed;
+
+    Rigidbody rb;
+
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentSpeed = rb.velocity;
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Instantiate(remains, transform.position, transform.rotation);
+        //    Rigidbody remainsRB = remains.GetComponent<Rigidbody>();
+        //    remainsRB.velocity = currentSpeed;
+        //    Destroy(gameObject);
+        //}
+	}
+
+    void OnCollisionEnter()
+    {
+        Instantiate(remains, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
