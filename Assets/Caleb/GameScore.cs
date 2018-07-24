f9d4e798cea48be13f4563cf86badbb6fe91a417
Caleb Seay <s179067@students.aie.edu.au>
2018-07-23 17:12:45 -0700

    Various menus, music as well as hud and pause

---
+++ Assets/Caleb/GameScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScore : MonoBehaviour {
+
+    Scoreholder tim;
+    public Text ending;
+    // Use this for initialization
+    void Start()
+    {
+        tim = FindObjectOfType<Scoreholder>();
+        ending.text = tim.score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tim = FindObjectOfType<Scoreholder>();
+        ending.text = tim.score.ToString();
+    }
+}
