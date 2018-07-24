3744d16adf0c884740539f1635f4b12f32cfd5e5
Zalia Tucker <porkypains@gmail.com>
2018-07-23 22:36:10 -0700

    fixing branch mistakes

---
+++ Assets/Caleb/HUDController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDController : MonoBehaviour {
+
+    public Slider health;
+    public Slider Throw;
+    public GameObject PMenu;
+
+	// Use this for initialization
+	void Start ()
+    {
+        health.value = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if (Input.GetMouseButtonDown(KeyCode.Mouse0) && (Throw.value < 1.0f))
+        // {
+
+        // }
+        gamepause();
+        if (PMenu.activeInHierarchy == false)
+        {
+            Time.timeScale = 1;
+        }
+	}
+
+   void gamepause()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Time.timeScale = 0;
+            PMenu.SetActive(true);
+        }
+    }
+}
