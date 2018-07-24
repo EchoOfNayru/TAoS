f25f1bde79af06953cd9ae6dec7d00bd9fefeeeb
Caleb Seay <s179067@students.aie.edu.au>
2018-07-24 11:07:22 -0700

    score script for player score

---
+++ Assets/Caleb/Scores.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scores : MonoBehaviour {
+    public int score;
+    public Scoreholder total;
+    int counter;
+	
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (counter >= 1)
+        {
+            return;
+        }
+        if (gameObject.tag == "Hammer")
+        {
+            total.score += score;
+            counter++;
+        }
+        
+    }
+}
