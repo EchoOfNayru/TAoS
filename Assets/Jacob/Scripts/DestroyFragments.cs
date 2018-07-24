f6996c9fb231055baeea183a52b58b43aeb37abe
Jacob M Miller <s179063@students.aie.edu.au>
2018-07-24 09:21:56 -0700

    Added a lifetime to the hammer fragments

---
+++ Assets/Jacob/Scripts/DestroyFragments.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyFragments : MonoBehaviour {
+
+    public float lifetime = 5.0f;
+	
+	void Update () {
+        lifetime -= Time.deltaTime;
+
+        if (lifetime <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
