54888591d6282a2523744d799d2d9f37086bcaf3
Zalia Tucker <porkypains@gmail.com>
2018-07-25 10:08:57 -0700

    high scores working

--- Assets/Caleb/Scoreholder.cs
+++ Assets/Caleb/Scoreholder.cs
@@ -9,6 +9,9 @@ public class Scoreholder : MonoBehaviour
     public int score = 0;
     public static Scoreholder instance;
 
+    public int[] highScores = new int[3];
+    //public string[] highScoresNames = new string[3];
+
 
     private void Awake()
     {
@@ -20,6 +23,12 @@ public class Scoreholder : MonoBehaviour
         {
             Destroy(this);
         }
+        highScores[0] = 5000;
+        //highScoresNames[0] = "CODY";
+        highScores[1] = 3000;
+        //highScoresNames[0] = "GOD";
+        highScores[2] = 1000;
+        //highScoresNames[0] = "BOB";
     }
 
     void Start()
